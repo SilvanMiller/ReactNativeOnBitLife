15c2a81a353d8625967607033caf5342476c0528
Silvan Miller <silvanmiller@gmail.com>
2022-12-02 17:45:01 -0300

    Estrutura da HomePage e renderização condicional - A2

---
+++ src/pages/HabitPage/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, TouchableOpacity, ScrollView, Image, Alert,Text, } from 'react-native';
+
+
+export default function HabitPage() {
+    const navigation = useNavigation();
+
+
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+                <View>
+                    <TouchableOpacity
+                        style={styles.backPageBtn}
+                        onPress={() => navigation.goBack}>
+                        <Image
+                            source={require("../../assets/icons/arrowBack.png")} />
+                    </TouchableOpacity>
+                    <View style={styles.mainContent}></View>
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    backPageBtn: {
+        width: 40,
+        height: 40,
+        margin: 25,
+    },
+    arrowBack: {
+        width: 40,
+        height: 40,
+    },
+    mainContent: {
+        width: 250,
+        alignSelf: "center",
+    },
+})
